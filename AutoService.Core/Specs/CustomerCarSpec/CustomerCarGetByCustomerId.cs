d55bbdf3e427a215be6d6ba759afb8a8e7e1eea0
Alex Biholar <Biholar0372@gmail.com>
2022-06-28 09:11:35 +0300

    Added files

---
+++ AutoService.Core/Specs/CustomerCarSpec/CustomerCarGetByCustomerId.cs
@@ -0,0 +1,16 @@
+﻿using Ardalis.Specification;
+using Autoservice.Infrastructure.Models;
+
+namespace AutoService.Core.Specs.CustomerCarSpec;
+
+public class CustomerCarGetByCustomerId:Specification<Customer>
+{
+
+    public CustomerCarGetByCustomerId()
+    {
+        Query.Include(x => x.CustomerCars)
+            .ThenInclude(x => x.Car)
+            .ThenInclude(x => x.ModelCar)
+            .ThenInclude(x=>x.Marke);
+    }
+}
---
+++ AutoService.WEBAPI/Dto/CustomerCarsDto.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using AutoService.Core.Mapper;
+using Autoservice.Infrastructure.Models;
+
+namespace AutoService.WEBAPI.Dto;
+
+public class CustomerCarsDto:IMap<Customer>
+{
+    public string Customer { get; set; }
+    public int CustomerId { get; set; }
+    public List<string> Cars { get; set; }
+    
+    
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<Customer, CustomerCarsDto>()
+            .ForMember(d => d.Customer, m =>
+                m.MapFrom(s => $"{s.FirstName} {s.SecondName}"))
+            .ForMember(d => d.Cars, m =>
+                m.MapFrom(s => s.CustomerCars.Select(x => $"{x.Car.ModelCar.Marke.Name} {x.Car.ModelCar.Name}")))
+            .ForMember(d=>d.CustomerId, m=>m.MapFrom(s=>s.Id));
+    }
+}
+
